d54c027d7e88fe38bf6b80c9efc9c786c3efda52
Petersen <apetersen@skylinetechnologies.com>
2018-05-31 12:51:18 -0500

    refactored nav reactions

--- src/hub/reactions/appReactions.ts
+++ src/hub/reactions/appReactions.ts
@@ -1,5 +1,6 @@
 import hub from "../../hub";
 import { NavRoute, parseRoute, updateUrl } from "../../navigation";
+import { _updateRoute } from "./navReactions";
 
 let onInit = async function() {
     _updateRoute(parseRoute())
@@ -12,44 +13,6 @@ let hideSplash = function() {
     setTimeout(() => document.getElementById("splash").style.display = "none", 300);
 }
 
-let onNavigate = function(route:NavRoute) {
-    return _updateRoute(route);
-}
 
-let onChangeArea = function(area:string) {
-    let targetArea = hub.state.nav.areas.find(a => a.key === area)
-    if (!targetArea) return null;
-    let route : NavRoute = {
-        area,
-        view: targetArea.activeView.name,
-        params: targetArea.activeView.params
-    };
-    return _updateRoute(route);
-}
-
-let validateRoute = function(route:NavRoute) : boolean {
-    if (!route || !route.area || !route.view) return false;
-    return !!(hub.state.nav.areas.find(a => a.key === route.area))
-}
-
-let _updateRoute = function(route:NavRoute, shouldUpdateUrl=true) {
-    if (!validateRoute) throw new Error("Navigation Error: Invalid Route");
-    let targetArea = hub.state.nav.areas.find(a => a.key === route.area);
-
-    targetArea.set({ activeView: { name: route.view, params: route.params }});
-    hub.state.nav.set({ activeArea: route.area});
-    if (shouldUpdateUrl) updateUrl(route);
-}
-
-// Handle Browser back button
-window.onpopstate = function(event) {
-    if (event.state && event.state.view) {
-        _updateRoute(parseRoute(), false);
-    } else {
-        window.history.back();
-    }
-};
 
 hub.on("app:init", onInit);
-hub.on("app:navigate", onNavigate);
-hub.on("nav:changeArea", onChangeArea);
