41341f54bdc0775d089bb980c55eb7566b371a2d
Petersen <apetersen@skylinetechnologies.com>
2018-06-01 16:38:09 -0500

    playing with manifest

--- src/AppShell/App.tsx
+++ src/AppShell/App.tsx
@@ -13,7 +13,7 @@ export default class App extends React.Component {
         hub.trigger("app:init");
     }
     render() {
-        return (
+        return ( 
     
             <div className="app">
                 <Location>
