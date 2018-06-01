acb5599df762bfc1d99f0a986cb5712ea67e1a7b
Petersen <apetersen@skylinetechnologies.com>
2018-06-01 16:58:33 -0500

    copied manifest and images post build

--- tasks/build.js
+++ tasks/build.js
@@ -1,5 +1,18 @@
+const fse = require('fs-extra')
 const fs = require("fs");
 const path = require("path");
 
 const fileContents = `/*    /index.html   200`
 fs.writeFileSync(path.join(__dirname, "..", "dist", "_redirects"), fileContents);
+
+fse.copySync('src/images', 'dist/images')
+
+const htmlExtras = `<link rel="manifest" href="/manifest.json">
+<link rel="icon" href="/images/icons/icon-72x72.png">
+<title>`
+fse.copySync("src/manifest.json", "dist/manifest.json");
+
+let html = fs.readFileSync("dist/index.html", "utf8");
+console.log(html);
+html = html.replace("<title>", htmlExtras);
+fs.writeFileSync("dist/index.html", html, "utf8");
